ceb93e63487bc709d1e84627d1647010c37bca3f
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2023-11-10 12:11:55 +0000

    renderならこれで

--- config/initializers/webauthn.rb
+++ config/initializers/webauthn.rb
@@ -1,7 +1,7 @@
 WebAuthn.configure do |config|
   # This value needs to match `window.location.origin` evaluated by
   # the User Agent during registration and authentication ceremonies.
-  config.origin = ENV.fetch('WEBAUTHN_ORIGIN') { "http://localhost:3000" }
+  config.origin = ENV.fetch('RENDER_EXTERNAL_URL') { "http://localhost:3000" }
 
   # Relying Party name for display purposes
   config.rp_name = "Rails Devise with webauthn-ruby Demo"

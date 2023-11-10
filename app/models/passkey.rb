4f54207b2ae14e117e5638f093f4bf6a3f4cf28b
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2023-11-10 04:49:52 +0000

    add webauthn models

---
+++ app/models/passkey.rb
@@ -0,0 +1,7 @@
+class Passkey < ApplicationRecord
+  belongs_to :webauthn_user
+
+  validates :external_id, presence: true, uniqueness: { scope: :webauthn_user_id }
+  validates :public_key, presence: true
+  validates :sign_count, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+end
